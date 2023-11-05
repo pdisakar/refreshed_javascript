135b6a721d66e335a3804683f7d039d5b2b8c4e3
Sakar Pudasaini <pdi.sakar@gmail.com>
2023-11-05 11:58:06 +0545

    Working On JS

---
+++ script.js
@@ -0,0 +1,20 @@
+// console.log(5);
+// console.log(55);
+
+// data type
+// console.log(typeof"JavaScript")
+// console.log(2011)
+// console.log('Woohoo! I love to code! #codecademy')
+// console.log(20.49)
+
+// Arithmetic Operators
+// console.log(3 + 4); 
+// console.log(5 - 1); 
+// console.log(4 * 2); 
+// console.log(9 / 3); 
+// console.log(11 % 3); 
+
+// String Concatenation
+// console.log('hi' + 'ya'); 
+// console.log('wo' + 'ah');
+// console.log('I love to ' + 'code.')
